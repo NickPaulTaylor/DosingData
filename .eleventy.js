6fd7f984c8e90a06cc8b0419fcf12fd48e2d6d25
NickPaulTaylor <nick.paul.taylor@gmail.com>
2025-04-21 11:39:48 +0100

    feat: Integrate Decap CMS

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,18 @@
-module.exports = function(eleventyConfig) { return { dir: { input: 'src' } } };
+module.exports = function(eleventyConfig) {
+
+  // Tell Eleventy to copy the 'admin' folder straight to the output
+  eleventyConfig.addPassthroughCopy("admin");
+
+  // Tell Eleventy to copy the 'src/assets' folder (where media_folder points)
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  // Add other configurations here if needed later
+
+  // Make sure Eleventy knows 'src' is the input folder
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
